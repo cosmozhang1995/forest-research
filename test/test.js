7d8e7b420cd841f1b4c291eb9b68d278649c3d5d
cosmozhang <zjzxz3006@126.com>
2014-11-07 22:27:22 +0800

    remove console logs

--- test/test.js
+++ test/test.js
@@ -1,18 +1,23 @@
 var forest = require('../index.js');
 
 // var data = forest.loadFile(__dirname + '/test-row-title.xlsx')[0];
-var data = forest.loadFile('/Users/zhangcosmo/Downloads/青冈次生林/乔木原始数据.xlsx')[0];
+var data = forest.loadFile('/Users/zhangcosmo/Downloads/青冈次生林/乔木原始数据.xlsx');
 
 // console.log(data);
 // console.log(data.getDetailTable());
 
-var output_data = [
-	{ name:'计算结果', data: data.getDetailTable() },
-	{ name:'统计：物种', data: data.getSpeciesSummaryTable() },
-	{ name:'统计：样地', data: data.getSampleSummaryTable() },
-	{ name:'统计：样地-物种', data: data.getSampleSpeciesSummaryTable() },
-	{ name:'统计结果', data: data.getSummaryTable() }
-];
+// var output_data = [
+// 	{ name:'计算结果', data: data.getDetailTable() },
+// 	{ name:'统计：物种', data: data.getSpeciesSummaryTable() },
+// 	{ name:'统计：样地', data: data.getSampleSummaryTable() },
+// 	{ name:'统计：样地-物种', data: data.getSampleSpeciesSummaryTable() },
+// 	{ name:'统计结果', data: data.getSummaryTable() }
+// ];
+
+var output_data = [];
+for (var i = 0; i < data.length; i++) {
+	output_data.push({ name:data[i].name, data:data[i].getSummaryTable() });
+}
 
 forest.putFile('./test/output.xlsx', output_data);
 
